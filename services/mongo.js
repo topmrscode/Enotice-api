27dd6d19b4a6c73181a06a44ae48aca67b36c3fd
Laura Baudean <laura.baudean@epitech.eu>
2020-11-12 21:58:08 +0100

    Modify URL of mongo

--- services/mongo.js
+++ services/mongo.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-
 const connect = () => {
   const DB_HOST = process.env.DB_HOST;
-  const DB_PORT = process.env.DB_PORT;
+  const DB_PORT = process.env.DB_PORT ? `:${process.env.DB_PORT}` : "";
   const DB_NAME = process.env.DB_NAME;
+  const DB_USER = process.env.DB_USER;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
 
-  const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}${DB_PORT}/${DB_NAME}`;
   const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
   return mongoose.connect(connectionString, mongoOptions);
 };
