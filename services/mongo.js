62955943417253409d70c5ca23e3731d23ce99b5
Laura Baudean <laura.baudean@epitech.eu>
2020-11-12 22:43:43 +0100

    Modify Mongo connect string

--- services/mongo.js
+++ services/mongo.js
@@ -4,10 +4,14 @@ const connect = () => {
   const DB_HOST = process.env.DB_HOST;
   const DB_PORT = process.env.DB_PORT ? `:${process.env.DB_PORT}` : "";
   const DB_NAME = process.env.DB_NAME;
-  const DB_USER = process.env.DB_USER;
+  const DB_USER = process.env.DB_USER ? process.env.DB_USER : "";
   const DB_PASSWORD = process.env.DB_PASSWORD;
 
-  const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}${DB_PORT}/${DB_NAME}`;
+  let DB_AUTH_STRING = "";
+  if (DB_USER) {
+    DB_AUTH_STRING = `${DB_USER}:${DB_PASSWORD}@`;
+  }
+  const connectionString = `mongodb://${DB_AUTH_STRING}${DB_HOST}${DB_PORT}/${DB_NAME}`;
   const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
   return mongoose.connect(connectionString, mongoOptions);
 };
